97c3d5b0753f91242030bd52aab19f1e2663a264
Phunicha <6522790227@g.siit.tu.ac.th>
2024-03-31 15:32:42 +0700

    aboutus

--- ninetys_liquid_lush/src/app/page.tsx
+++ ninetys_liquid_lush/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import MainpicHome from "../components/MainpicHome";
-
+import Grid from "./components/Grid";
 export default function Home() {
   return (
     <main className="flex flex-col justify-between items-center p-24 min-h-screen">
